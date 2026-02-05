67501374019abfd56ed8f404aadb5251c7a26623
mshamar1997 <Mshamar1997@gmail.com>
2026-02-05 11:43:01 -0600

    cart button

--- startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", () => {
+    const cartBtn = document.getElementById("cartBtn");
+
+    cartBtn.addEventListener("click", () => {
+      // If you're already inside /pages/, go to cart.html in the same folder.
+      // If you're on home (index.html), go into /pages/cart.html.
+      const path = window.location.pathname;
+      const isInPagesFolder = path.includes("/pages/");
+
+      window.location.href = isInPagesFolder ? "cart.html" : "pages/cart.html";
+    });
+  });
